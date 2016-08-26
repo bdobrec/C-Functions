519e24af37098fb9a650896c98e85b4afa5e4e12
Brandon Dobrec <bdobrec@Brandons-MacBook-Pro-3.local>
2016-08-25 18:43:13 -0700

    changes to language

--- ft_cat.h
+++ ft_cat.h
@@ -4,8 +4,8 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-void	jp_putchar(char c);
-void	jp_putstr(char *str);
-int		jp_display_file(char *filename);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_display_file(char *filename);
 
 #endif
