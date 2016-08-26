23a933ac4e32437393ecd57d56b4dcd35952f14b
Brandon Dobrec <bdobrec@Brandons-MacBook-Pro-3.local>
2016-08-25 18:42:31 -0700

    the header file to accompany

---
+++ ft_cat.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+# include <unistd.h>
+# include <fcntl.h>
+
+void	jp_putchar(char c);
+void	jp_putstr(char *str);
+int		jp_display_file(char *filename);
+
+#endif
