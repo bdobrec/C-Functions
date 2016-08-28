341c63518bb6df0b48a62e8a3632d01fe55b05e2
Brandon Dobrec <bdobrec@Brandons-MacBook-Pro-3.local>
2016-08-27 18:13:10 -0700

    beginning supporting functions for rush02 at the piscine

---
+++ print_response.c
@@ -0,0 +1,10 @@
+void	ft_print(int rush, int row, int col)
+{
+	ft_putstr("rush-0");
+	ft_putnbr(rush);
+	ft_putstr("] [");
+        ft_putnbr(row);
+	ft_putstr("] [");
+        ft_putnbr(col);
+	ft_putstr("]");
+}
---
+++ support_functions.c
@@ -0,0 +1,29 @@
+#include "header.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
